38739ea9f80f77b9b153017a0a66357e86f6e4d7
Shashank <shashank022@gmail.com>
2021-10-27 18:01:00 -0500

    minor updates

---
+++ src/components/Slider.jsx
@@ -0,0 +1,102 @@
+import { ArrowLeftOutlined, ArrowRightOutlined, Info } from '@material-ui/icons';
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: white;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+    font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const Slider = () => {
+    return (
+        <Container>
+            <Arrow direction="left" >
+                <ArrowLeftOutlined/>
+            </Arrow>
+            <Wrapper>
+                <ImgContainer>
+                    <Image src="https://i.ibb.co/0t7fwfr/fashion.jpg"/>
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>SUMMER SALE...</Title>
+                    <Desc>DON'T COMPROMISE ON STYLE..!! GET FLAT 30% OFF THE NEW ARRIVALS</Desc>
+                    <Button>SHOP NOW..</Button>
+                </InfoContainer>
+            </Wrapper>
+            <Arrow direction="right" >
+                <ArrowRightOutlined/>
+            </Arrow>
+        </Container>
+    )
+}
+
+export default Slider
