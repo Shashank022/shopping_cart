c06ee3760c2472cfe014ef2a63456ad073b5ed86
Shashank022 <shashank022@gmail.com>
2021-10-28 13:23:24 -0500

    updated code changes

---
+++ src/components/Catogories.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@material-ui/core'
+import React from 'react'
+
+const Catogories = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default Catogories
---
+++ src/components/CatogoryItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CatogoryItem = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CatogoryItem
