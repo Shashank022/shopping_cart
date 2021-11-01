9134c2c6119c93d718d935cd4bb33e3bc98347a0
Shashank022 <shashank022@gmail.com>
2021-10-31 21:01:18 -0500

    Added footer to the code

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,9 @@
+import { Announcement } from '@material-ui/icons';
 import React from 'react';
 import styled from "styled-components";
+import Navbar from '../components/Navbar';
+import Newsletter from '../components/Newsletter';
+import Footer from '../components/Footer';
 
 
 const Container = styled.div``;
@@ -7,7 +11,10 @@ const Container = styled.div``;
 const Product = () => {
     return (
         <Container>
-
+            <Navbar />
+            <Announcement />
+            <Newsletter />
+            <Footer />
         </Container>
     )
 }
